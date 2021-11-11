76e87638d24d489979f4e1ee60bb299a2d1347fc
tk <takahiro59327guitar@gmail.com>
2021-11-11 16:32:21 +0900

    knex設定,test(get,post)完了

--- index.js
+++ index.js
@@ -8,7 +8,58 @@ const knex = require("knex")(config.db);
 const express = require("express");
 const app = express();
 
-//server開始
- app.listen(config.express.port, () => {
-    console.log(`Server up and listening on port ${config.express.port}`);
+const setupServer = () => {
+  app.use(express.json());
+  app.use(express.static("web"));
+
+  //確認用
+  app.get("/api", (req, res) => {
+    res.send("We did it!");
   });
+
+  //チーム、メダルデータ取得
+  app.get("/team/medals", (req, res) => {
+    knex("olympicsdb")
+      .select("team", "gold")
+      .from("medals")
+      .then(result => {
+        res.send(result);
+      });
+  });
+
+  //チームデータ取得
+  app.get("/team/list", (req, res) => {
+    const { limit } = req.query;
+    knex("olympicsdb")
+      .select("team")
+      .from("medals")
+      .then(result => {
+        let ret = result.map(val => val.team);
+        if (limit > 0) {
+          ret = ret.slice(0, limit);
+        }
+        res.send(ret);
+      });
+
+    //データ追加
+    app.post("/team/list", (req, res) => {
+      const team = Object.values(req.body)[0];
+      knex("olympicsdb")
+        .select("team")
+        .from("medals")
+        .then(result => {
+          let ret = result.map(val => val.team);
+          ret.push(team);
+          res.send(ret);
+        });
+    });
+  });
+  return app;
+};
+
+const server = setupServer();
+server.listen(config.express.port, () => {
+  console.log(`Server up and listening on port ${config.express.port}`);
+});
+
+module.exports = { setupServer };
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,46 @@
-const { expect, assert } = require("chai");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { setupServer } = require("../");
 const config = require("../config");
 const knex = require("knex")(config.db);
+chai.should();
+chai.use(chaiHttp);
+
+const server = setupServer();
+
+describe("API server", () => {
+  let request;
+  beforeEach(() => {
+    request = chai.request(server).keepOpen();
+  });
+  describe("teams", () => {
+    it("should return api messge", async () => {
+      const res = await request.get("/api");
+      res.text.should.equal("We did it!");
+      request.close();
+    });
+  });
+
+  describe("teams", () => {
+    it("should get all teams", async () => {
+      const res = await request.get("/team/list?limit=3");
+      const expected = JSON.stringify([
+        "United States of America",
+        "China",
+        "Japan"
+      ]);
+      res.text.should.equal(expected);
+      request.close();
+    });
+  });
+
+  describe("POST /team/list", () => {
+    it("should get all teams", async () => {
+      const res = await request.post("/team/list").send({ team: "npKingdom" });
+      const actual = JSON.parse(res.text);
+      const actualArr = actual.filter(val => val === "npKingdom");
+      actualArr.length.should.equal(1);
+      request.close();
+    });
+  });
+});
